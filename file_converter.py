519a136bb25eac09f7643bb41a79e2c0ad5caecb
partys-over <radic4lly@gmail.com>
2022-12-24 16:34:00 -0800

    features added, new mp3 playlist function

--- file_converter.py
+++ file_converter.py
@@ -1,6 +1,10 @@
+import os
 from moviepy.editor import VideoFileClip
 
 def convert_to_mp3(filename):
     clip = VideoFileClip(filename)
+    clip2 = (filename)
+    print('STRING-' + clip2 + " -\n")
     clip.audio.write_audiofile(filename[:-4] + ".mp3")
+    os.remove(clip2)
     clip.close()

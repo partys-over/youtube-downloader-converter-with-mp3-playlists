c2996ead688718308eae6d9d1a520df9b2578b91
partys-over <radic4lly@gmail.com>
2022-12-24 16:37:21 -0800

    removing old mp4 files upon conversion

--- file_converter.py
+++ file_converter.py
@@ -4,7 +4,7 @@ from moviepy.editor import VideoFileClip
 def convert_to_mp3(filename):
     clip = VideoFileClip(filename)
     clip2 = (filename)
-    print('STRING-' + clip2 + " -\n")
+    #print('STRING-' + clip2 + " -\n") used for debugging
     clip.audio.write_audiofile(filename[:-4] + ".mp3")
     os.remove(clip2)
     clip.close()
